dbb735053a1663ccaeae883dc674a23a28e3df17
Sohail Khan <sohail.a.khan@ericsson.com>
2023-12-17 23:26:15 +0100

    POC for JWT
    Change-Id: I40cf2d094fee7c487b41a76a6b8846e2c32ed724

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sohail-9098/ms-user-auth
+
+go 1.20
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.0
+	github.com/gorilla/mux v1.8.1
+)
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	Username string
+	Password string
+}
+
+var secretKey = []byte("secret-key")
+
+func createToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+	if err != nil {
+		return err
+	}
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+	return nil
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var u User
+	json.NewDecoder(r.Body).Decode(&u)
+	fmt.Printf("The user requested value %v", u)
+
+	if u.Username == "Chek" && u.Password == "qwerty" {
+		tokenString, err := createToken(u.Username)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Errorf("No username found")
+		}
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, tokenString)
+		return
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "invalid credentials")
+	}
+}
+
+func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	tokenString := r.Header.Get("Authorization")
+	if tokenString == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "Missing authorization header")
+		return
+	}
+	tokenString = tokenString[len("Bearer "):]
+	err := verifyToken(tokenString)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "invalid token")
+		return
+	}
+	fmt.Fprint(w, "Welcome to protected area!s")
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/login", LoginHandler).Methods("POST")
+	router.HandleFunc("/protected", ProtectedHandler).Methods("GET")
+
+	fmt.Println("Starting Server")
+	err := http.ListenAndServe("localhost:4000", router)
+	if err != nil {
+		fmt.Println("Could not start the server", err)
+	}
+}

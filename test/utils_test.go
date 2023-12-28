2668b7cdac433c6ecfff3b218236c48c84292634
Sohail Khan <sohail.a.khan@ericsson.com>
2023-12-28 13:17:10 +0100

    refactor test
    Change-Id: Id444bd1bb7ebd3005e50a45762738e493bce2fdd

--- test/utils_test.go
+++ test/utils_test.go
@@ -1,12 +1,18 @@
-package util
+package test
 
 import (
 	"testing"
 
 	"github.com/sohail-9098/ms-user-auth/util"
+	"github.com/stretchr/testify/require"
 )
 
-func TestDb_OpenFile(t *testing.T) {
+func TestDb_OpenCloseFile(t *testing.T) {
+	// Arrage
 	configFileName := "../config.yaml"
-	util.OpenFile(configFileName)
+	// Act
+	file := util.OpenFile(configFileName)
+	err := file.Close()
+	// Assert
+	require.NoError(t, err, "error closing file")
 }
